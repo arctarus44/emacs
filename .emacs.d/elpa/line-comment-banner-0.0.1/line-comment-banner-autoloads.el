ba6486753a019e7ed3f0ae4a4db907c34f0a2629
arctarus <arthur.dewarumez@gmail.com>
2016-09-29 00:48:39 +0200

    Cleaning the branch.

--- .emacs.d/elpa/line-comment-banner-0.0.1/line-comment-banner-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; line-comment-banner-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("line-comment-banner-pkg.el" "line-comment-banner.el")
-;;;;;;  (21001 23659 514426 667000))
-
-;;;***
-
-(provide 'line-comment-banner-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; line-comment-banner-autoloads.el ends here
--- .emacs.d/elpa/line-comment-banner-0.0.1/line-comment-banner-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "line-comment-banner" "0.0.1" "Comment a line in a banner style." (quote nil))
